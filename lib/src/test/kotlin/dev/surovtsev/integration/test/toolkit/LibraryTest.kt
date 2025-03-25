3aaf3f1f5de629639a136fa4d089288bb34fe9e0
o.surovtsev <o.surovtsev@spd-ukraine.com>
2025-03-26 01:41:49 +0200

    init

---
+++ lib/src/test/kotlin/dev/surovtsev/integration/test/toolkit/LibraryTest.kt
@@ -0,0 +1,11 @@
+package dev.surovtsev.integration.test.toolkit;
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class ContextTest {
+    @Test
+    fun contextTest() {
+        assertTrue(true, "someLibraryMethod should return 'true'")
+    }
+}
